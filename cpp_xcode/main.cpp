4c331cea4a8601763c0f5412b67d8e0c25ec5640
Lin Yang <liny@outlook.com>
2017-10-12 17:47:45 +1100

    Initial Commit

---
+++ cpp_xcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_xcode
+//
+//  Created by Lin on 12/10/17.
+//  Copyright © 2017 Runskey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
